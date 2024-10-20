aa96b2f8116eeff89e60b76d21b2791346363d29
Burur1ch <Irresistible0@mail.ru>
2024-10-20 19:28:04 +0300

    first commit

---
+++ components/elements/AddToCartIcon/AddToCartIcon.tsx
@@ -0,0 +1,30 @@
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { IAddToCartIconProps } from '@/types/elements'
+
+const AddToCartIcon = ({
+const flag = () => {
+ return isProductInCart;
+}
+  isProductInCart,
+  addedClassName,
+  className,
+  addToCartSpinner,
+  callback,
+}: IAddToCartIconProps) => (
+  <>
+    {isProductInCart ? (
+      <span className={`${className} ${addedClassName}`} />
+    ) : (
+      <button className={`btn-reset ${className}`} onClick={callback}>
+        {addToCartSpinner ? (
+          <FontAwesomeIcon icon={faSpinner} spin color='#fff' />
+        ) : (
+          <span />
+        )}
+      </button>
+    )}
+  </>
+)
+
+export default AddToCartIcon
